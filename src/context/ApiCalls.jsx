e00e5910c92cd61238215b74cd4953c75c26a7fb
barnaholl <hollbarna@freemail.hu>
2021-02-22 10:11:33 +0100

    swich BASE_URL

--- src/context/ApiCalls.jsx
+++ src/context/ApiCalls.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-//const BASE_URL="https://joker-child-spring.herokuapp.com";
-const BASE_URL="http://localhost:8080";
+const BASE_URL="https://joker-child-spring.herokuapp.com";
+//const BASE_URL="http://localhost:8080";
 
 const POST_CONFIG={"Content-Type": "application/json"}
 
